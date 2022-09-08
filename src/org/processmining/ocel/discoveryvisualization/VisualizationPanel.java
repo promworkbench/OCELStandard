f7bb2d7a56c4ac4fb4b4a93919d96f5b75e539fa
fit-alessandro-berti <a.berti@pads.rwth-aachen.de>
2022-09-08 10:02:55 +0000

    Fixed color for the activity nodes
    git-svn-id: https://svn.win.tue.nl/repos/prom/Packages/OCELStandard/Trunk@46672 a68f48d4-8d00-424f-b812-3f22a534a02a

--- src/org/processmining/ocel/discoveryvisualization/VisualizationPanel.java
+++ src/org/processmining/ocel/discoveryvisualization/VisualizationPanel.java
@@ -1076,9 +1076,9 @@ class VisualizationTab extends JPanel {
 				int height = 30 * stringHeight;
 
 				int cc = 125 + 125*(this.model.MAX_INDIPENDENT_ACT_COUNT - activity.getValue(this.panel.controlTab.getSelectedIndex()))/(this.model.MAX_INDIPENDENT_ACT_COUNT - this.model.MIN_INDIPENDENT_ACT_COUNT);
-				String hex = String.format("#%02x%02x%02x", cc, cc, cc);
+				//String hex = String.format("#%02x%02x%02x", cc, cc, cc);
 				
-				Object activityObject = graph.insertVertex(parent, activity.activity, label, 150, 150, width, height, "fontSize=18;fillColor="+hex);
+				Object activityObject = graph.insertVertex(parent, activity.activity, label, 150, 150, width, height, "fontSize=18;fillColor=lightblue");
 				activityIndipendent.put(activity, activityObject);
 				invActivityIndipendent.put(activityObject, activity);
 				activityIndipendentString.put(activity.activity, activityObject);
